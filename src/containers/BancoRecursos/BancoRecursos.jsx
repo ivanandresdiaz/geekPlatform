ad87512e15e67d50c2f6cc75e4fab98b411af11c
FriendGlak <camilojheans@gmail.com>
2021-08-15 12:19:44 -0500

    Rutas panel corregidas.

--- src/containers/BancoRecursos/BancoRecursos.jsx
+++ src/containers/BancoRecursos/BancoRecursos.jsx
@@ -3,14 +3,18 @@ import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { getFirestoreCategoryData } from '../../actions/bancoRecursosActions';
 import { getCategories, getSubCategories, getCategoryData } from '../../reducers/bancoRecursosReducer';
-import { getFullName, getUserId } from '../../reducers/authReducer';
+import { getFullName, getRole, getUserId } from '../../reducers/authReducer';
 import CardRecursoAcademico from '../../uiComponents/CardRecursoAcademico/CardRecursoAcademico';
+import NavbarTeacher from '../../components/Structure/NavbarTeacher';
 import { DivContainerRecursos } from './styledBancoRecursos';
 import AddNewAcademicResource from '../../components/AddNewAcademicResource/AddNewAcademicResource';
 import CreateNewCategory from '../../components/CreateNewCategory/CreateNewCategory';
 import ListarRecursosAcademicos from '../../uiComponents/ListarRecursosAcademicos/ListarRecursosAcademicos';
+import NavbarAdmin from '../../components/Structure/NavbarAdmin';
+import NavbarStudent from '../../components/Structure/NavbarStudent';
 
 const BancoRecursos = () => {
+  const role = useSelector(getRole);
   const dispatch = useDispatch();
   const categoryData = useSelector(getCategoryData);
   const categories = useSelector(getCategories);
@@ -25,6 +29,15 @@ const BancoRecursos = () => {
   };
   return (
     <>
+      {role === 'teacher' && (
+        <NavbarTeacher />
+      )}
+      {role === 'admin' && (
+        <NavbarAdmin />
+      )}
+      {role === 'student' && (
+        <NavbarStudent />
+      )}
       <h1>Banco de Recursos academicos</h1>
       <h3>Categorias</h3>
       <div>
